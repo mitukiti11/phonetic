839653ad2e0d0feaffb68321ae74e31bcea54600
mitukiti <buffalo.rage@gmail.com>
2017-04-16 22:13:59 +0900

    init

--- src/NATO.php
+++ src/NATO.php
@@ -1,117 +1,119 @@
 <?php
-    namespace Phonetic;
 
-    class NATO {
+namespace Phonetic;
 
-        public static $CODE = array(
-            CODE::ALPHA    => CODE::A,
-            CODE::BRAVO    => CODE::B,
-            CODE::CHARLIE  => CODE::C,
-            CODE::DELTA    => CODE::D,
-            CODE::EKHO     => CODE::E,
-            CODE::FOX      => CODE::F,
-            CODE::GOLF     => CODE::G,
-            CODE::HOTEL    => CODE::H,
-            CODE::INDIA    => CODE::I,
-            CODE::JULIETT  => CODE::J,
-            CODE::KILO     => CODE::K,
-            CODE::LIMA     => CODE::L,
-            CODE::MIKE     => CODE::M,
-            CODE::NOVEMBER => CODE::N,
-            CODE::OSCER    => CODE::O,
-            CODE::PAPA     => CODE::P,
-            CODE::QUEBEC   => CODE::Q,
-            CODE::ROMEO    => CODE::R,
-            CODE::SIERRA   => CODE::S,
-            CODE::TANGO    => CODE::T,
-            CODE::UNIFORM  => CODE::U,
-            CODE::VICTOR   => CODE::V,
-            CODE::WHISKEY  => CODE::W,
-            CODE::XRAY     => CODE::X,
-            CODE::YANKEE   => CODE::Y,
-            CODE::ZULU     => CODE::Z,
+class NATO
+{
+    public static $CODE = array(
+        CODE::ALPHA => CODE::A,
+        CODE::BRAVO => CODE::B,
+        CODE::CHARLIE => CODE::C,
+        CODE::DELTA => CODE::D,
+        CODE::EKHO => CODE::E,
+        CODE::FOX => CODE::F,
+        CODE::GOLF => CODE::G,
+        CODE::HOTEL => CODE::H,
+        CODE::INDIA => CODE::I,
+        CODE::JULIETT => CODE::J,
+        CODE::KILO => CODE::K,
+        CODE::LIMA => CODE::L,
+        CODE::MIKE => CODE::M,
+        CODE::NOVEMBER => CODE::N,
+        CODE::OSCER => CODE::O,
+        CODE::PAPA => CODE::P,
+        CODE::QUEBEC => CODE::Q,
+        CODE::ROMEO => CODE::R,
+        CODE::SIERRA => CODE::S,
+        CODE::TANGO => CODE::T,
+        CODE::UNIFORM => CODE::U,
+        CODE::VICTOR => CODE::V,
+        CODE::WHISKEY => CODE::W,
+        CODE::XRAY => CODE::X,
+        CODE::YANKEE => CODE::Y,
+        CODE::ZULU => CODE::Z,
 
-            NUMBER::ZERO   => NUMBER::ZERO,
-            NUMBER::ONE    => NUMBER::ONE,
-            NUMBER::TWO    => NUMBER::TWO,
-            NUMBER::THREE  => NUMBER::THREE,
-            NUMBER::FOUR   => NUMBER::FOUR,
-            NUMBER::FIVE   => NUMBER::FIVE,
-            NUMBER::SIX    => NUMBER::SIX,
-            NUMBER::SEVEN  => NUMBER::SEVEN,
-            NUMBER::EIGHT  => NUMBER::EIGHT,
-            NUMBER::NINE   => NUMBER::NINE,
-        );
-    }
+        NUMBER::ZERO => NUMBER::ZERO,
+        NUMBER::ONE => NUMBER::ONE,
+        NUMBER::TWO => NUMBER::TWO,
+        NUMBER::THREE => NUMBER::THREE,
+        NUMBER::FOUR => NUMBER::FOUR,
+        NUMBER::FIVE => NUMBER::FIVE,
+        NUMBER::SIX => NUMBER::SIX,
+        NUMBER::SEVEN => NUMBER::SEVEN,
+        NUMBER::EIGHT => NUMBER::EIGHT,
+        NUMBER::NINE => NUMBER::NINE,
+    );
+}
 
 
-    public class NUMBER {
-        const ZERO    = 0;
-        const ONE     = 1;
-        const TWO     = 2;
-        const THREE   = 3;
-        const FOUR    = 4;
-        const FIVE    = 5;
-        const SIX     = 6;
-        const SEVEN   = 7;
-        const EIGHT   = 8;
-        const NINE    = 9;
-    }
+class NUMBER
+{
+    const ZERO    = 0;
+    const ONE     = 1;
+    const TWO     = 2;
+    const THREE   = 3;
+    const FOUR    = 4;
+    const FIVE    = 5;
+    const SIX     = 6;
+    const SEVEN   = 7;
+    const EIGHT   = 8;
+    const NINE    = 9;
+}
 
-    public class CODE
-    {
-        const A = "ALPHA";
-        const B = "BRAVO";
-        const C = "CHARLIE";
-        const D = "DELTA";
-        const E = "ECHO";
-        const F = "FOX";
-        const G = "GOLF";
-        const H = "HOTEL";
-        const I = "INDIA";
-        const J = "JULIETT";
-        const K = "KILO";
-        const L = "LIMA";
-        const M = "MIKE";
-        const N = "NOVEMBER";
-        const O = "OSCER";
-        const P = "PAPA";
-        const Q = "QUEBEC";
-        const R = "ROMEO";
-        const S = "SIERRA";
-        const T = "TANGO";
-        const U = "UNIFORM";
-        const V = "VICTOR";
-        const W = "WHISKEY";
-        const X = "XRAY";
-        const Y = "YANKEE";
-        const Z = "ZULU";
+class CODE
+{
+    const A = "ALPHA";
+    const B = "BRAVO";
+    const C = "CHARLIE";
+    const D = "DELTA";
+    const E = "ECHO";
+    const F = "FOX";
+    const G = "GOLF";
+    const H = "HOTEL";
+    const I = "INDIA";
+    const J = "JULIETT";
+    const K = "KILO";
+    const L = "LIMA";
+    const M = "MIKE";
+    const N = "NOVEMBER";
+    const O = "OSCER";
+    const P = "PAPA";
+    const Q = "QUEBEC";
+    const R = "ROMEO";
+    const S = "SIERRA";
+    const T = "TANGO";
+    const U = "UNIFORM";
+    const V = "VICTOR";
+    const W = "WHISKEY";
+    const X = "XRAY";
+    const Y = "YANKEE";
+    const Z = "ZULU";
 
-        CONST ALPHA     = "A";
-        CONST BRAVO     = "B";
-        CONST CHARLIE   = "C";
-        CONST DELTA     = "D";
-        CONST EKHO      = "E";
-        CONST FOX       = "F";
-        CONST GOLF      = "G";
-        CONST HOTEL     = "H";
-        CONST INDIA     = "I";
-        CONST JULIETT   = "J";
-        CONST KILO      = "K";
-        CONST LIMA      = "L";
-        CONST MIKE      = "M";
-        CONST NOVEMBER  = "N";
-        CONST OSCER     = "O";
-        CONST PAPA      = "P";
-        CONST QUEBEC    = "Q";
-        CONST ROMEO     = "R";
-        CONST SIERRA    = "S";
-        CONST TANGO     = "T";
-        CONST UNIFORM   = "U";
-        CONST VICTOR    = "V";
-        CONST WHISKEY   = "W";
-        CONST XRAY      = "X";
-        CONST YANKEE    = "Y";
-        CONST ZULU      = "Z";
-    }
+    CONST ALPHA = "A";
+    CONST BRAVO = "B";
+    CONST CHARLIE = "C";
+    CONST DELTA = "D";
+    CONST EKHO = "E";
+    CONST FOX = "F";
+    CONST GOLF = "G";
+    CONST HOTEL = "H";
+    CONST INDIA = "I";
+    CONST JULIETT = "J";
+    CONST KILO = "K";
+    CONST LIMA = "L";
+    CONST MIKE = "M";
+    CONST NOVEMBER = "N";
+    CONST OSCER = "O";
+    CONST PAPA = "P";
+    CONST QUEBEC = "Q";
+    CONST ROMEO = "R";
+    CONST SIERRA = "S";
+    CONST TANGO = "T";
+    CONST UNIFORM = "U";
+    CONST VICTOR = "V";
+    CONST WHISKEY = "W";
+    CONST XRAY = "X";
+    CONST YANKEE = "Y";
+    CONST ZULU = "Z";
+}
 
